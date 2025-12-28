9b0bffa931859d0859db442f0b7153ea58ff66fe
Gennarox <ivanmateog@hotmail.com>
2025-12-28 17:32:01 +0000

    fix

--- scripts/silver/products_transformations.py
+++ scripts/silver/products_transformations.py
@@ -132,7 +132,13 @@ def transform_chunk(chunk: pd.DataFrame) -> pd.DataFrame:
         chunk["product_id"].astype(str).str.lower().str.strip() + "_" +
         chunk["product_name"].astype(str).str.lower().str.strip() + "_" + 
         chunk["supermarket"].astype(str).str.lower().str.strip() #+ "_" + 
-        #chunk["snapshot_date"].astype(str)
+    )
+
+    chunk["product_hash"] = (
+        chunk["product_id"].astype(str).str.lower().str.strip() + "_" +
+        chunk["product_name"].astype(str).str.lower().str.strip() + "_" + 
+        chunk["supermarket"].astype(str).str.lower().str.strip() + "_" + 
+        chunk["snapshot_date"].astype(str)
     )
 
     return chunk
